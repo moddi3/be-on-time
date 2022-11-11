cc59910db3c0938959692c95942f0ab7277ab034
Vlad Ivanov <v.ivanov@sendpulse.com>
2022-11-11 21:01:53 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 
 import { trpc } from '../utils/trpc';
 
@@ -14,9 +15,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
     pageProps: { session, ...pageProps },
   }) => {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+            <Analytics />
+        </>
     );
 };
 
